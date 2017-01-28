9b4ecfe7ca348c56a9a32a9873de56c2e027a231
Instantium <nadler21+instantium@gmail.com>
2017-01-28 18:10:15 +0100

    Added Universal ImageLoader Library and loading Product images

--- app/src/main/java/com/fluffylighty/productscroller/MainApplication.java
+++ app/src/main/java/com/fluffylighty/productscroller/MainApplication.java
@@ -3,6 +3,8 @@ package com.fluffylighty.productscroller;
 import android.app.Application;
 
 import com.fluffylighty.productscroller.controller.MainController;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 /**
  * Created by Nico Adler on 28.01.17.
@@ -15,6 +17,10 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+        .build();
+        ImageLoader.getInstance().init(config);
 
         mainController = new MainController();
     }
--- app/src/main/java/com/fluffylighty/productscroller/view/HorizontalAdapter.java
+++ app/src/main/java/com/fluffylighty/productscroller/view/HorizontalAdapter.java
@@ -6,10 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fluffylighty.productscroller.R;
 import com.fluffylighty.productscroller.model.Product;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 
@@ -34,6 +36,7 @@ public class HorizontalAdapter extends ArrayAdapter<Product> {
             holder = new ViewHolder();
             holder.productNameTextView = (TextView) convertView.findViewById(R.id.horizontal_listview_list_item_name_text_view);
             holder.productPricingTextView = (TextView) convertView.findViewById(R.id.horizontal_listview_list_item_pricing_text_view);
+            holder.productImageView = (ImageView) convertView.findViewById(R.id.horizontal_listview_list_item_image_view);
 
             convertView.setTag(holder);
         } else {
@@ -45,6 +48,10 @@ public class HorizontalAdapter extends ArrayAdapter<Product> {
         if (currentItem != null) {
             holder.productNameTextView.setText(currentItem.getName());
             holder.productPricingTextView.setText(currentItem.getPrice() + "");
+
+            String imageUrl = currentItem.getImages()[0].getUrl();
+
+            ImageLoader.getInstance().displayImage(imageUrl, holder.productImageView);
         }
 
         return convertView;
@@ -53,5 +60,6 @@ public class HorizontalAdapter extends ArrayAdapter<Product> {
     private class ViewHolder {
         public TextView productPricingTextView;
         public TextView productNameTextView;
+        public ImageView productImageView;
     }
 }
